e033b69ed87db86e407d4ff89d7a736aff8e8aab
obadah2001 <91162594+obadah2001@users.noreply.github.com>
2023-11-13 19:10:27 -0600

    Update pipython.py

--- pipython.py
+++ pipython.py
@@ -1,10 +1,9 @@
-import time
-import paho.mqtt.client as mqtt
-import ssl
-import json
-import _thread
-import RPi.GPIO as GPIO
-import time
+import time #my code
+import paho.mqtt.client as mqtt #my code
+import ssl #my code
+import json #my code
+import _thread #my code
+import RPi.GPIO as GPIO #my code
 import board
 import adafruit_dht
 import serial
@@ -22,17 +21,17 @@ from PIL import ImageTk,Image
 import RPi.GPIO as GPIO
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
-client = mqtt.Client()
+client = mqtt.Client() # my code
 
 vMax = 13.66
 vMin = 12
 switch = 1
 
 def init():
-    client.on_connect = on_connect
-    client.tls_set(ca_certs='./rootCA.pem', certfile='./certificate.pem.crt', keyfile='./private.pem.key', tls_version=ssl.PROTOCOL_SSLv23)
-    client.tls_insecure_set(True)
-    client.connect("aa7bx2yvfl8gx-ats.iot.us-east-1.amazonaws.com", 8883, 60) #Taken from REST API endpoint - Use your own.
+    client.on_connect = on_connect # my code
+    client.tls_set(ca_certs='./rootCA.pem', certfile='./certificate.pem.crt', keyfile='./private.pem.key', tls_version=ssl.PROTOCOL_SSLv23) # my code
+    client.tls_insecure_set(True) # my code
+    client.connect("aa7bx2yvfl8gx-ats.iot.us-east-1.amazonaws.com", 8883, 60) #Taken from REST API endpoint - Use your own. 
 
 def onExit():
     print("bye")
@@ -82,8 +81,8 @@ def calculateBatteryPercent(voltage):
     
     
 
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+def on_connect(client, userdata, flags, rc): #my code
+    print("Connected with result code "+str(rc)) #my code
 
 #init()
 #atexit.register(onExit)
@@ -93,7 +92,7 @@ sheet = workbook.active
 f = open("timemarks.txt","a")
 f.write(datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
 f.close()
-while True:
+while True:  #my code
     data = ser.readline().decode().strip()
     jsonStr = jsonConstruct(data)
     print(jsonStr)
@@ -102,8 +101,8 @@ while True:
     f.write(jsonStr)
     f.write("\n")
     f.close()
-    client.publish("device/data", payload=(jsonStr) , qos=0, retain=False)
-    time.sleep(5)
-client.loop_forever()
+    client.publish("device/data", payload=(jsonStr) , qos=0, retain=False) #my code
+    time.sleep(5) #my code
+client.loop_forever() #my code
 
 
